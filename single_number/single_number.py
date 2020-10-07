32a5df26fbcae961a3e05a6596f86ab043390c46
DJ Coleman <DJ@DJs-MacBook-Pro.attlocal.net>
2020-10-06 19:48:41 -0700

    completed 2 problems

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -1,11 +1,26 @@
 '''
+Given a non-empty array of integers where every element appears twice except for one. Find that single number. You may assume that there will _always_ be _one_ odd-number-out and every other number in the input shows up exactly twice.
+
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
+- loop through the array of integers 
+  - check if there's a double number
+    - if so, break
+    - if find single number, return that number
 '''
+from iteration_utilities import duplicates
+
+
 def single_number(arr):
     # Your code here
-
-    pass
+    single = set()
+    for i in arr:
+        if i in single:
+            j = list(single)
+            res = str(j)[1:-1]
+            return res
+        else:
+            single.add(i)
 
 
 if __name__ == '__main__':
