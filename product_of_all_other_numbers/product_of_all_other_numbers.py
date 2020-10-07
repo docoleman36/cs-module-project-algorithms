00999b91046fa6b044350776433699d2d7e1d7e7
DJ Coleman <DJ@DJs-MacBook-Pro.attlocal.net>
2020-10-06 21:13:23 -0700

    attempting product of all nums

--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -1,16 +1,28 @@
 '''
+Write a function that receives an array of integers and returns an array consisting of the product of all numbers in the array except the number at that index.
+
 Input: a List of integers
 Returns: a List of integers
+
+Notes:
+- loop through all the numbers 
+- multiple each number in the array except for that index
 '''
+
+
 def product_of_all_other_numbers(arr):
     # Your code here
-
-    pass
+    nums = 1
+    for i in arr:
+        nums = arr[i:] * arr[i:]
+        return nums
 
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
     # arr = [1, 2, 3, 4, 5]
-    arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8, 9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
+    arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8,
+           9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
 
-    print(f"Output of product_of_all_other_numbers: {product_of_all_other_numbers(arr)}")
+    print(
+        f"Output of product_of_all_other_numbers: {product_of_all_other_numbers(arr)}")
